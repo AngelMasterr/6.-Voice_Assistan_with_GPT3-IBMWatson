5d8e0d9eb3318631c6d3cd86b46f11b9dfb0b613
AngelD <angelduarteing@gmail.com>
2023-03-25 20:01:44 -0500

    mas

--- prueba.py
+++ prueba.py
@@ -1,15 +1,26 @@
-from collections import Counter
-text = "creo que a veces es la gente de la que nadie espera nada, la que hace cosas que nadie puede imaginar"
-list_text = list(text.lower().split())
-count_text = Counter(list_text)
-print(dict(count_text))
+def minTime(machines, goal):
+    from decimal import Decimal
+    machines.sort()
+    sum_mach = sum(1/x for x in machines)
+    days = (goal/sum_mach)       
+    cont = 1
+    print(days)
+    while True:
+        cont += 1
+        if cont == 100: break
+        min_mac = min([x - (days % x) for x in machines]) 
+        sum_day = sum(days//x for x in machines)
+        if sum_day >= goal:
+            break              
+        else: days += min_mac
+        print(sum_day)
+    return(int(days)) 
 
-text = "creo que a veces es la gente de la que nadie espera nada, la que hace cosas que nadie puede imaginar"
-list_text = list(text.lower().split())
-dict_t = dict()
-for i in list_text:
-    if i in dict_t:
-        dict_t[i] += 1
-    else:
-        dict_t[i] = 1
-print(dict_t)
+with open("Archivo//datos.txt","r") as dat:
+    machines = list(map(int, dat.readline().split()))
+    goal = 313369020
+
+print(minTime(machines, goal))
+
+# 340655733624
+# 340628671584
